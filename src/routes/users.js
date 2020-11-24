4848f0ea58fd6fd1cef4e3d8e479c6e292c21c70
prmstrgt <pramestiregitaa@gmail.com>
2020-11-24 13:34:28 +0700

    add revision

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const route = require('express').Router()
 const usersControllers = require('../controllers/users')
 
-route.patch('/set-profile/:id', usersControllers.setProfile)
+route.patch('/set-profile', usersControllers.setProfile)
 
 module.exports = route
